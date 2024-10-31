42cee71c93900c7bce13978cc654f9162256aa3b
fengshuzi <fengshuzi@sina.com>
2024-10-31 15:08:56 +0800

    重新整理

--- src/.vuepress/navbar/index.ts
+++ src/.vuepress/navbar/index.ts
@@ -2,7 +2,7 @@ import { navbar } from "vuepress-theme-hope";
 
 export const zhNavbar = navbar([
   "/",
-  { text: "alfred小手段", icon: "discover", link: "feynman/alfred/" },
-  { text: "随笔杂谈", icon: "discover", link: "feynman/diary/" },
+  { text: "alfred小手段", icon: "discover", link: "/feynman/alfred/" },
+  { text: "随笔杂谈", icon: "discover", link: "/feynman/diary/" },
 
 ]);
