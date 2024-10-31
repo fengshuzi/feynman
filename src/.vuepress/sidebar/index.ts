d6d5cd7221c8617620f9ce94d3486eed5b2e874e
fengshuzi <fengshuzi@sina.com>
2024-10-31 15:05:37 +0800

    重新整理

--- src/.vuepress/sidebar/index.ts
+++ src/.vuepress/sidebar/index.ts
@@ -4,26 +4,10 @@ export const zhSidebar = sidebar({
   "/": [
     "",
     {
-      text: "并发编程",
-      icon: "note",
-      collapsible: true,
-      link: "concurrent/",
-      prefix: "concurrent/",
-      children: "structure",
-    },
-    {
       text: "alfred小手段",
       icon: "creative",
       prefix: "feynman/alfred",
       collapsible: true,
-      link: "jvm/",
-      children: "structure",
-    },
-    {
-      text: "读书笔记",
-      icon: "note",
-      collapsible: true,
-      prefix: "feynman/reading/",
       children: "structure",
     },
     {
