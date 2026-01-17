08933bb3a65e93e3b6d2d9ae44a0540b218085e5
lizhifeng <lizhifeng@zuzuche.ccom>
2026-01-17 19:32:05 +0800

    文章发布

--- src/.vuepress/navbar/index.ts
+++ src/.vuepress/navbar/index.ts
@@ -3,6 +3,6 @@ import { navbar } from "vuepress-theme-hope";
 export const zhNavbar = navbar([
   "/",
   { text: "alfred小手段", icon: "discover", link: "/feynman/alfred/" },
-  { text: "随笔杂谈", icon: "discover", link: "/feynman/diary/" },
+  { text: "obsidian插件开发", icon: "discover", link: "/feynman/obsidian/" },
 
 ]);
