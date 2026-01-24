67681474e3e3af5ab4187010d7b6114b60dc994d
lizhifeng <lizhifeng@zuzuche.ccom>
2026-01-24 22:38:58 +0800

    去掉没用的文章

--- src/.vuepress/navbar/index.ts
+++ src/.vuepress/navbar/index.ts
@@ -1,8 +1,7 @@
 import { navbar } from "vuepress-theme-hope";
 
 export const zhNavbar = navbar([
-  "/",
+  { text: "首页", icon: "home", link: "/" },
   { text: "alfred小手段", icon: "discover", link: "/feynman/alfred/" },
   { text: "obsidian插件开发", icon: "discover", link: "/feynman/obsidian/" },
-
 ]);
